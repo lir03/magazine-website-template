a087fc38f2eac65693595abb944f3c42579a2b75
Matthew Wisen <lir03@yahoo.com>
2022-09-15 12:25:02 +0800

    adding dist

---
+++ dist/js/53.c12c2b09.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwebsite_template"]=self["webpackChunkwebsite_template"]||[]).push([[53],{4053:function(e,n,t){t.r(n),t.d(n,{default:function(){return p}});var a=t(6252);const r=e=>((0,a.dD)("data-v-4d971fda"),e=e(),(0,a.Cn)(),e),s={class:"container"},u=r((()=>(0,a._)("h1",null,"About Us",-1))),d=r((()=>(0,a._)("p",null,"Magazine information",-1))),i=[u,d];function c(e,n){return(0,a.wg)(),(0,a.iD)("div",s,i)}var l=t(3744);const o={},f=(0,l.Z)(o,[["render",c],["__scopeId","data-v-4d971fda"]]);var p=f}}]);
+//# sourceMappingURL=53.c12c2b09.js.map
